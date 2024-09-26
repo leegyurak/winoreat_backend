2e3c2c7f9fc8b08ebc193de57d330f4813927e8d
leegyurak <dev@devgyurak.com>
2024-09-27 03:52:53 +0900

    CHG: settings

--- winoreat/settings.py
+++ winoreat/settings.py
@@ -27,9 +27,9 @@ elif os.environ.get('ENV') == 'prod':
     SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
